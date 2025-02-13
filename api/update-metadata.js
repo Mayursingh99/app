1293f1e4e3ff5ece14c5237cfe5027ab323178c3
Mayursingh99 <mayursinh.appsrow@gmail.com>
2025-02-13 15:30:28 +0530

    Updated update-metadata.js function

--- api/update-metadata.js
+++ api/update-metadata.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method Not Allowed' });
